8735b644d31f271825778aa60ce936a9c5454cf0
AndreyShyshkin <shyshkinandrey06@gmail.com>
2024-02-09 00:04:09 +0200

    updateMain

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Group from './functions/group'
 import Home from './home/Home'
 import './main.styles.scss'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const AppRouter = () => (
+	<BrowserRouter>
+		<Routes>
+			<Route path='/' element={<Home />} />
+			<Route path='/functions/group' element={<Group />} />
+		</Routes>
+	</BrowserRouter>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
 	<React.StrictMode>
-		<Router>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/functions/group' element={<Group />} />
-			</Routes>
-		</Router>
+		<AppRouter />
 	</React.StrictMode>
 )
